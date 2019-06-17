c6da0edf2bad6c49cf1972bdfbef5c64862eec65
erozonachi <erozonachi.james@gmail.com>
2019-06-17 13:03:38 +0100

    Complete the decrement case logic in reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,7 +14,7 @@ export default (state = initialState, action) => {
     case INCREMENT:
       return {count: state.count + 1};
     case DECREMENT:
-    // Fill in the body of this case
+      return {count: state.count - 1};
     default:
       return state;
   }
